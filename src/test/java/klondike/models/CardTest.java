6e572400bbaf659fa7bd5e36e16cef60a5577893
Thomas Loor <pipos_rgt@hotmail.com>
2016-10-04 22:09:24 -0500

    Edit TestCard

--- src/test/java/klondike/models/CardTest.java
+++ src/test/java/klondike/models/CardTest.java
@@ -12,14 +12,15 @@ public class CardTest {
 
     @Before
     public void initialize() {
-        card1 = new Card(Number.EIGHT,Suit.BRILLO,Boolean.FALSE);
-        //card2 = new Card(Number.ACE,Suit.CORAZON,Boolean.FALSE);
+        card1 = new Card(Number.EIGHT,Suit.BRILLO,Boolean.TRUE);
+        
     }
 	
-	//@Test
-	//public void testSetHidden() {
-	//	assertTrue(Boolean.FALSE=card1.setHidden(false));
-//	}
+	@Test
+	public void testSetHidden() {
+		card1.setHidden(false);
+		assertFalse(card1.isHidden());
+	}
 	@Test
 	public void testGetSuit() {
 		assertEquals(Suit.BRILLO, card1.getSuit());
@@ -32,13 +33,13 @@ public class CardTest {
 
 	@Test
 	public void testIsHidden() {
-		assertFalse(card1.isHidden());
+		assertTrue(card1.isHidden());
 	}
 
 	@Test
 	public void testToString() {
-		 assertEquals("[EIGHT,BRILLO]", card1.toString());
-	      
+		card1.setHidden(true);
+		assertTrue(card1.toString().equals("[]"));
 	}
 
 }
